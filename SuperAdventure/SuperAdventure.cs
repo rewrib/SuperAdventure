47f79a803837bc4a11600b4ff42c9caa00cfdf7a
rewrib <birwer.ali@gmx.de>
2020-04-03 14:28:30 +0200

    updating player's level

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -20,14 +20,11 @@ namespace SuperAdventure
         {
             InitializeComponent();
 
-            _player = new Player(10, 10, 20, 0, 1);
+            _player = new Player(10, 10, 20, 0);
             MoveTo(World.LocationByID(World.LOCATION_ID_HOME));
             _player.Inventory.Add(new InventoryItem(World.ItemByID(World.ITEM_ID_RUSTY_SWORD), 1));
 
-            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
-            lblGold.Text = _player.Gold.ToString();
-            lblExperience.Text = _player.ExperiencePoints.ToString();
-            lblLevel.Text = _player.Level.ToString();
+            UpdatePlayerStats();
         }
         private void SuperAdventure_Load(object sender, EventArgs e)
         {
@@ -213,6 +210,9 @@ namespace SuperAdventure
                 btnUsePotion.Visible = false;
             }
 
+
+            // Refresh player's stats
+            UpdatePlayerStats();
             // Refresh player's inventory list
             UpdateInventoryListInUI();
 
@@ -324,6 +324,16 @@ namespace SuperAdventure
                 cboPotions.SelectedIndex = 0;
             }
         }
+
+
+        private void UpdatePlayerStats()
+        {
+            // Refresh player information and inventory controls
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
         private void btnUseWeapon_Click(object sender, EventArgs e)
         {
             // Get the currently selected weapon from the cboWeapons ComboBox
@@ -404,6 +414,7 @@ namespace SuperAdventure
                 lblExperience.Text = _player.ExperiencePoints.ToString();
                 lblLevel.Text = _player.Level.ToString();
 
+                UpdatePlayerStats();
                 UpdateInventoryListInUI();
                 UpdateWeaponListInUI();
                 UpdatePotionListInUI();
